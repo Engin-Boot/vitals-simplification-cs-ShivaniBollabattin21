cd55ec3259781a829fd260f3330e049f27eec23e
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-08-28 13:31:23 +0530

    Add files via upload

---
+++ VitalsSimplification/BPMCheker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VitalsSimplification
+{
+    class BPMCheker
+    {
+        public static bool checkBpm(float bpm)
+        {
+            if (bpm >= 70 && bpm <= 150)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
