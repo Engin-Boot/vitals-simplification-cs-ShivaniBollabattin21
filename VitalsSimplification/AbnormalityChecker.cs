725d1aacf1e264e2b455f2fe5bdfcd8fe613949c
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-08-28 22:02:49 +0530

    Add files via upload

---
+++ VitalsSimplification/AbnormalityChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vitals_simplification
+{
+    class AbnormalityChecker
+    {
+        public static void checkAbnormalBPM(float bpm)
+        {
+            if(bpm<70)
+            {
+                Console.WriteLine(bpm + " BPM is Low");
+            }
+            else if(bpm>150)
+            {
+                Console.WriteLine(bpm + " BPM is High");
+            }
+        }
+
+        public static void checkAbnormalSpo2(float spo2)
+        {
+            if(spo2<95)
+            {
+                Console.WriteLine(spo2+" Oxygen Saturation is Low");
+            }
+            else if(spo2>100)
+            {
+                Console.WriteLine(spo2 + " Oxygen Saturation is High");
+            }
+        }
+
+        public static void checkAbnormalRespRate(float respRate)
+        {
+            if(respRate<30)
+            {
+                Console.WriteLine(respRate + " Respiratory rate is Low");
+            }
+            else if(respRate>95)
+            {
+                Console.WriteLine(respRate + " Respiratory rate is High");
+            }
+        }
+    }
+}
