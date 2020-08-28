81dc9b0c242f058dfb8f24389854204cff810131
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-08-28 21:11:11 +0530

    Update VitalsChecker.cs

--- VitalsSimplification/VitalsChecker.cs
+++ VitalsSimplification/VitalsChecker.cs
@@ -10,16 +10,22 @@ namespace VitalsSimplification
 {
     class VitalsChecker
     {
-        public static bool vitalsAreOk(float bpm, float spo2, float respRate)
+         public static bool vitalsAreOk(float bpm, float spo2, float respRate)
         {
             bool vitalsCheck = false;
-            bool condition1 = BPMCheker.checkBpm(bpm) && SPO2Checker.checkSpo2(spo2) ;
-            bool condition2 = condition1 && RespRateChecker.checkRespRate(respRate);
-            if(condition2)
+            bool condition = checkVitals(bpm,spo2,respRate);
+            if(condition)
             {
                 vitalsCheck = true;
             }
             return vitalsCheck;
+        }
+
+        static bool checkVitals(float bpm, float spo2, float respRate)
+        {
+            bool condition1 = BPMCheker.checkBpm(bpm) && SPO2Checker.checkSpo2(spo2);
+            bool condition2 = condition1 && RespRateChecker.checkRespRate(respRate);
+            return condition2;
         }
     }
 }
