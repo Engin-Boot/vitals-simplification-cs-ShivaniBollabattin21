32b324bc07a9887c66957bbf31e8ecf7ae744958
ShivaniBollabattin21 <68583125+ShivaniBollabattin21@users.noreply.github.com>
2020-09-07 21:18:37 +0530

    Update AbnormalityChecker.cs

--- VitalsSimplification/AbnormalityChecker.cs
+++ VitalsSimplification/AbnormalityChecker.cs
@@ -1,45 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace vitals_simplification
+namespace VitalsSimplification
 {
     class AbnormalityChecker
     {
-        public static void checkAbnormalBPM(float bpm)
+        public static string CheckAbnormalVitalandReturnMessage(string vitalName,float vitalReading,float upperLimit, float lowerLimit)
         {
-            if(bpm<70)
+            string message = vitalName + "is Normal";
+            if(vitalReading<lowerLimit)
             {
-                Console.WriteLine(bpm + " BPM is Low");
+                message = vitalName + "is Low";
             }
-            else if(bpm>150)
+            else if(vitalReading>upperLimit)
             {
-                Console.WriteLine(bpm + " BPM is High");
+                message = vitalName + "is High";
             }
+            return message;
         }
-
-        public static void checkAbnormalSpo2(float spo2)
-        {
-            if(spo2<95)
-            {
-                Console.WriteLine(spo2+" Oxygen Saturation is Low");
-            }
-            else if(spo2>100)
-            {
-                Console.WriteLine(spo2 + " Oxygen Saturation is High");
-            }
-        }
-
-        public static void checkAbnormalRespRate(float respRate)
-        {
-            if(respRate<30)
-            {
-                Console.WriteLine(respRate + " Respiratory rate is Low");
-            }
-            else if(respRate>95)
-            {
-                Console.WriteLine(respRate + " Respiratory rate is High");
-            }
-        }
+       
     }
 }
